215fe926a6143e53924a2b62679c05e3ab611a7a
geekyouth <forsupergeeker@gmail.com>
2022-11-22 11:38:44 +0800

    ☺ 补充脚本

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo -s;
+cd;
+
+# https://ius.io/setup
+yum install -y \
+vim net-tools wget epel-release yum-utils htop tcping \
+https://repo.ius.io/ius-release-el7.rpm \
+https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
+git236;
+
+git clone https://github.com/geekyouth/vagrant.git;
+cd vagrant; ls alh;
+
+cd vagrant_cluster/; ls -alh;
+#cat Vagrantfile;
+vargant up; vargant status;
+
+#################################################
+# curl -sL https://raw.githubusercontent.com/geekyouth/vagrant/main/start.sh | bash
