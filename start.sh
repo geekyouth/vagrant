7698d253a681f1ec534b66dfff2bb936f82dbb5d
geekyouth <forsupergeeker@gmail.com>
2022-11-22 12:08:34 +0800

    🙄🙄🙄

--- start.sh
+++ start.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-sudo -s;
-cd;
+sudo -s
+cd
 
-# https://ius.io/setup
 yum install -y \
-vim net-tools wget epel-release yum-utils htop tcping \
-https://repo.ius.io/ius-release-el7.rpm \
-https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
-git236;
+  vim net-tools wget htop tcping \
+  https://repo.ius.io/ius-release-el7.rpm \
+  https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-if [[ -d "vagrant" ]];then
-  echo "vagrant directory already exists, backuping...";
-  mv -f vagrant vagrant.bak;
+git --version
+if [[ $? == 0 ]]; then
+  echo "Git is already installed"
+else
+  echo "Installing Git"
+  # https://ius.io/setup
+  yum install -y https://repo.ius.io/ius-release-el7.rpm git236
 fi
 
-git clone https://github.com/geekyouth/vagrant.git;
-cd ./vagrant; ls -alh;
+if [[ -d "vagrant" ]]; then
+  echo "vagrant directory already exists, backuping..."
+  mv -f vagrant vagrant.bak
+fi
 
-cd ./cluster; ls -alh;
-#cat Vagrantfile;
-vargant up; vargant status;
+git clone https://github.com/geekyouth/vagrant.git
+cd ./vagrant
+ls -alh
+
+cd ./cluster
+ls -alh
+
+#################################################
+cat Vagrantfile
+#################################################
+
+vagrant up
+vagrant status
 
 #################################################
 # curl -sSL https://raw.githubusercontent.com/geekyouth/vagrant/main/start.sh | sh -x
